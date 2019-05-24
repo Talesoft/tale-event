08d6f36c99c2cf4ad563e005b3bd08e86385f961
Torben Köhn <t.koehn@outlook.com>
2019-05-24 18:06:10 +0200

    - Function fix

--- src/functions.php
+++ src/functions.php
@@ -5,15 +5,16 @@ namespace Tale;
 use Psr\EventDispatcher\ListenerProviderInterface;
 use Tale\Event\ListenerProvider\ChainListenerProvider;
 use Tale\Event\ListenerProvider\ClassMapListenerProvider;
+use Tale\Event\ListenerProvider\ReflectionListenerProvider;
 
 function event_dispatcher(ListenerProviderInterface $listenerProvider = null): EventDispatcher
 {
     return new EventDispatcher($listenerProvider ?? event_listener_provider_reflection());
 }
 
-function event_listener_provider_reflection(array $listeners = []): ClassMapListenerProvider
+function event_listener_provider_reflection(array $listeners = []): ReflectionListenerProvider
 {
-    return new ClassMapListenerProvider($listeners);
+    return new ReflectionListenerProvider($listeners);
 }
 
 function event_listener_provider_class_map(array $classMap): ClassMapListenerProvider
